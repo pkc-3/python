a828c32c312201fbbc8c4efe18881dbc7308be5a
pkc-3 <pkc_3@naver.com>
2021-04-27 15:10:10 +0900

    2021/04/27 파이썬 IO test 실습 추가

--- Ch02/2_4_string.py
+++ Ch02/2_4_string.py
@@ -18,3 +18,30 @@ print('name * 3 :', name*3)
 #문자열 길이
 msg = 'hello World'
 print('msg 길이 :', len(msg))
+
+#문자열 인덱스
+print('msg 1번째 문자:', msg[0])
+print('msg 6번째 문자:', msg[6])
+print('msg -1번째 문자:', msg[-1])
+print('msg -5번째 문자:', msg[-5])
+
+#문자열 자르기(substring)
+print('msg 0~5까지 문자열:', msg[0:5])
+print('msg 처음부터 5까지 문자열:', msg[:5])
+print('msg 6~11까지 문자열:', msg[6:11])
+print('msg 6부터 마지막까지 문자열:', msg[6:])
+
+#문자열 분리
+people='김유신|김춘추|장보고|강감찬|이순신'
+p1,p2,p3,p4,p5 = people.split('|')
+print('p1:',p1)
+print('p2:',p2)
+print('p3:',p3)
+print('p4:',p4)
+print('p5:',p5)
+
+#문자열 이스케이프
+print('서울\n대전\n대구\n부산\n광주')
+print('서울\t대전\t대구\t부산\t광주')
+print('저는 \'홍길동\' 입니다.')
+print("저는 '홍길동' 입니다.")
---
+++ Ch02/2_5_IOTest.py
@@ -0,0 +1,27 @@
+"""
+날짜:2021/04/27
+이름:고현석
+내용:파이썬 표준입출력 실습 교재 p48
+"""
+
+#파이썬 표준 출력
+print('hello', end='!')#끝에 느낌표 넣기
+print('python')
+
+print('010','1234','1111', sep='-')#중간에 작대기 넣기 separate값
+
+#파이썬 표준 입력
+num = input('숫자 입력:')
+
+print('입력된 숫자:',num)
+print('num type:',type(num))
+
+#입력받은 문자열을 숫자로 전환
+result = int(num)
+print('num type:',type(result))
+
+#서식문자
+print('%d년 %d월 %d일 %s요일' % (2021, 4, 27,'화'))
+
+#포맷문자
+print('이름: {}, 나이: {}, 주소:{}'.format('김유신', 23,'김해시'))
